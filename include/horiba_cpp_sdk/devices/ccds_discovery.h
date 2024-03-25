64f71641f515566319682b33f0b32ee47ceec4a7
Samuel Gauthier <samuel.gauthier@zuehlke.com>
2024-03-25 16:28:45 +0100

    add missing doxygen doc in device discovery headers

--- include/horiba_cpp_sdk/devices/ccds_discovery.h
+++ include/horiba_cpp_sdk/devices/ccds_discovery.h
@@ -9,14 +9,33 @@
 #include <nlohmann/json.hpp>
 
 namespace horiba::devices {
+/**
+ * @brief Represents a discovery of CCD cameras on the ICL
+ */
 class ChargeCoupledDevicesDiscovery : public DeviceDiscovery {
  public:
+  /**
+   * @brief Builds a CCD discovery
+   *
+   * @param communicator The communicator that will talk to the ICL
+   */
   explicit ChargeCoupledDevicesDiscovery(
       std::shared_ptr<horiba::communication::Communicator> communicator);
   ~ChargeCoupledDevicesDiscovery() override = default;
 
+  /**
+   * @brief Executes the CCD discovery
+   *
+   * @param error_on_no_devices Whether to throw an exception or not if no
+   * devices are detected
+   */
   void execute(bool error_on_no_devices) noexcept(false) override;
 
+  /**
+   * @brief CCDs that have been discovered after calling the execute() function
+   *
+   * @return The detected CCDs
+   */
   std::vector<std::shared_ptr<single_devices::ChargeCoupledDevice>>
   charge_coupled_devices() const;
 
--- include/horiba_cpp_sdk/devices/device_discovery.h
+++ include/horiba_cpp_sdk/devices/device_discovery.h
@@ -3,10 +3,19 @@
 
 namespace horiba::devices {
 
+/**
+ * @brief Interface for device discoveries on the ICL
+ */
 class DeviceDiscovery {
  public:
   virtual ~DeviceDiscovery() = default;
 
+  /**
+   * @brief Executes the device discovery
+   *
+   * @param error_on_no_devices whether to throw an exception or not if no
+   * devices are detected
+   */
   virtual void execute(bool error_on_no_devices) = 0;
 };
 } /* namespace horiba::devices */
