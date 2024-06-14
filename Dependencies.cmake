88d4dfbd3f7d9ef4eaac77cf039ad515e4fe8f87
Samuel Gauthier <samuel.gauthier@zuehlke.com>
2024-06-14 16:58:07 +0200

    fix formatting

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -11,37 +11,32 @@ function(horiba_cpp_sdk_setup_dependencies)
   CPMAddPackage(
     NAME Ccache.cmake
     GITHUB_REPOSITORY TheLartians/Ccache.cmake
-    VERSION 1.2
-  )
+    VERSION 1.2)
 
   CPMAddPackage("gh:TheLartians/Format.cmake@1.8.1")
 
   if(NOT TARGET fmtlib::fmtlib)
-    cpmaddpackage("gh:fmtlib/fmt#9.1.0")
+    CPMAddPackage("gh:fmtlib/fmt#9.1.0")
   endif()
 
   if(NOT TARGET spdlog::spdlog)
-    cpmaddpackage(
-      NAME
-      spdlog
-      VERSION
-      1.11.0
-      GITHUB_REPOSITORY
-      "gabime/spdlog"
-      OPTIONS
-      "SPDLOG_FMT_EXTERNAL ON")
+    CPMAddPackage(
+      NAME spdlog
+      VERSION 1.11.0
+      GITHUB_REPOSITORY "gabime/spdlog"
+      OPTIONS "SPDLOG_FMT_EXTERNAL ON")
   endif()
 
   if(NOT TARGET Catch2::Catch2WithMain)
-    cpmaddpackage("gh:catchorg/Catch2@3.3.2")
+    CPMAddPackage("gh:catchorg/Catch2@3.3.2")
   endif()
 
   if(NOT TARGET CLI11::CLI11)
-    cpmaddpackage("gh:CLIUtils/CLI11@2.3.2")
+    CPMAddPackage("gh:CLIUtils/CLI11@2.3.2")
   endif()
 
   if(NOT TARGET tools::tools)
-    cpmaddpackage("gh:lefticus/tools#update_build_system")
+    CPMAddPackage("gh:lefticus/tools#update_build_system")
   endif()
 
   if(NOT TARGET Boost::beast)
@@ -50,22 +45,18 @@ function(horiba_cpp_sdk_setup_dependencies)
       VERSION 1.84.0
       URL https://github.com/boostorg/boost/releases/download/boost-1.84.0/boost-1.84.0.tar.xz
       URL_HASH SHA256=2e64e5d79a738d0fa6fb546c6e5c2bd28f88d268a2a080546f74e5ff98f29d0e
-      OPTIONS "BOOST_ENABLE_CMAKE ON" "BOOST_INCLUDE_LIBRARIES static_string\\\;logic\\\;endian\\\;container\\\;asio"
-    )
+      OPTIONS "BOOST_ENABLE_CMAKE ON" "BOOST_INCLUDE_LIBRARIES static_string\\\;logic\\\;endian\\\;container\\\;asio")
 
     CPMAddPackage(
       NAME BoostBeast
       VERSION 1.84.0
       URL https://github.com/boostorg/beast/archive/refs/tags/boost-1.84.0.tar.gz
       URL_HASH SHA256=2e3fdd5fb79f23a1f6d239e747af21633f4236983f9e03eb21d2da77453b07de
-      OPTIONS "BOOST_ENABLE_CMAKE ON" "BOOST_INCLUDE_LIBRARIES container\\\;asio"
-    )
+      OPTIONS "BOOST_ENABLE_CMAKE ON" "BOOST_INCLUDE_LIBRARIES container\\\;asio")
   endif()
-
 
   if(NOT TARGET nlohmann_json::nlohmann_json)
     CPMAddPackage("gh:nlohmann/json@3.11.3")
   endif()
-
 
 endfunction()
